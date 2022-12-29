956511eff25a43fc3241f5a108ff430c1582393c
Ning Li <lining04111223@gmail.com>
2022-12-28 22:37:00 -0800

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
   },
   plugins: [],
-}
+};
