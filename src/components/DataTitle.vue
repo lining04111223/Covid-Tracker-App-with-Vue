3c733b477de91545d5e8ef1e6e0d4c030a49ba0f
Ning Li <lining04111223@gmail.com>
2022-12-29 19:09:51 -0800

    connect to api

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-center"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="home">Hello World</div>
+  <main
+    v-if="loading"
+    class="flex flex-col align-center justify-center text-center"
+  >
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching data</div>
+    <img :src="loadingImage" class="w-24 m-auto" />
+  </main>
+  <main v-else>{{ info.data.Date }}</main>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HomeView",
   components: {},
+  data() {
+    return {
+      loading: true,
+      info: null,
+      errored: false,
+      loadingImage: require("../assets/kOnzy.gif"),
+    };
+  },
+  created() {
+    axios
+      .get("https://api.covid19api.com/summary")
+      .then((response) => {
+        this.info = response;
+        console.log(this.info.data.Date);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
 };
 </script>
